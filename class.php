e4fb0e2a4a144a424993e49e68117e01daf3eddb
Jeremy Green <greenhornet79@gmail.com>
2019-06-06 08:39:10 -0600

    update check to work with new lp restrictions

--- class.php
+++ class.php
@@ -11,239 +11,170 @@
  *
  * @since 1.0.0
  */
-if ( ! class_exists( 'Leaky_Paywall_IP_Exceptions' ) ) {
+
+class Leaky_Paywall_IP_Exceptions {
 	
-	class Leaky_Paywall_IP_Exceptions {
-		
-		/**
-		 * Class constructor, puts things in motion
-		 *
-		 * @since 1.0.0
-		 */
-		function __construct() {
-					
-			$settings = $this->get_settings();
-			
-			add_action( 'wp', array( $this, 'process_requests' ), 5 );
-			
-			add_action( 'leaky_paywall_after_subscriptions_settings', array( $this, 'settings_div' ) );
-			add_filter( 'leaky_paywall_update_settings_settings', array( $this, 'update_settings_div' ), 10, 2 );
-			
-		}
-		
-		function process_requests() {
-			
-			global $leaky_paywall;
-			
-			$settings = $this->get_settings();
-			$ip_address = leaky_paywall_get_ip_address();
-			$ip_address_long = (float)sprintf( "%u", ip2long( $ip_address ) );
-			
-			$allowed_ips = explode( "\n", $settings['allowed_ip_addresses'] );
-
-			if ( leaky_paywall_ip_allows_access() ) {
-				remove_action( 'wp', array( $leaky_paywall, 'process_requests' ) );
-				remove_action( 'wp', array( $leaky_paywall, 'process_content_restrictions' ) );
-				add_filter( 'leaky_paywall_filter_is_restricted', '__return_false', 10, 2 );
-				return;
-			}
-			
-			/*
-			foreach( $allowed_ips as $ip ) {
-				$ip = trim( $ip );
-				if ( $ip === $ip_address ) {
-					remove_action( 'wp', array( $leaky_paywall, 'process_requests' ) );
-					remove_action( 'wp', array( $leaky_paywall, 'process_content_restrictions' ) );
-					add_filter( 'leaky_paywall_filter_is_restricted', '__return_false', 10, 2 );
-					return true;
-				}	
-				if ( false !== strpos( $ip, '*' ) ) {
-					$start = (float)sprintf( "%u", ip2long( trim( str_replace( '*', '0', $ip ) ) ) );
-					$end = (float)sprintf( "%u", ip2long( trim( str_replace( '*', '255', $ip ) ) ) );
-					if ( $ip_address_long >= $start && $ip_address_long <= $end ) {
-						remove_action( 'wp', array( $leaky_paywall, 'process_requests' ) );
-						remove_action( 'wp', array( $leaky_paywall, 'process_content_restrictions' ) );
-						add_filter( 'leaky_paywall_filter_is_restricted', '__return_false', 10, 2 );
-						return true;
-					}
-				}
-				if ( false !== stripos( $ip, 'x' ) ) {
-					$start = (float)sprintf( "%u", ip2long( trim( str_ireplace( 'x', '0', $ip ) ) ) );
-					$end = (float)sprintf( "%u", ip2long( trim( str_ireplace( 'x', '255', $ip ) ) ) );
-					if ( $ip_address_long >= $start && $ip_address_long <= $end ) {
-						remove_action( 'wp', array( $leaky_paywall, 'process_requests' ) );
-						remove_action( 'wp', array( $leaky_paywall, 'process_content_restrictions' ) );
-						add_filter( 'leaky_paywall_filter_is_restricted', '__return_false', 10, 2 );
-						return true;
-					}
-				}
-				if ( false !== stripos( $ip, '-' ) ) {
-					list( $start, $end ) = explode( '-', $ip, 2 );
-					$start = (float)sprintf( "%u", ip2long( trim( $start ) ) );
-					$end = (float)sprintf( "%u", ip2long( trim( $end ) ) );
-					if ( $ip_address_long >= $start && $ip_address_long <= $end ) {
-						remove_action( 'wp', array( $leaky_paywall, 'process_requests' ) );
-						remove_action( 'wp', array( $leaky_paywall, 'process_content_restrictions' ) );
-						add_filter( 'leaky_paywall_filter_is_restricted', '__return_false', 10, 2 );
-						return true;
-					}
-				}
-				if ( false !== strpos( $ip, '/' ) ) {
-					list( $net, $mask ) = explode( '/', $ip, 2 );
-					$net = ip2long( trim( $net ) );
-					$mask = ~( ( 1 << ( 32 - trim( $mask ) ) ) - 1 );
-					$ip_net = $ip_address_long & $mask;
-					if ( $ip_net === $net ) {
-						remove_action( 'wp', array( $leaky_paywall, 'process_requests' ) );
-						remove_action( 'wp', array( $leaky_paywall, 'process_content_restrictions' ) );
-						add_filter( 'leaky_paywall_filter_is_restricted', '__return_false', 10, 2 );
-						return true;
-					}
-				}
-			}
-			*/
-			
-		}
-
-		public function ip_allows_access() 
-		{
-			
-			$settings = $this->get_settings();
-			$ip_address = leaky_paywall_get_ip_address();
-			$ip_address_long = (float)sprintf( "%u", ip2long( $ip_address ) );
-			
-			$allowed_ips = explode( "\n", $settings['allowed_ip_addresses'] );
-
-			foreach( $allowed_ips as $ip ) {
-				$ip = trim( $ip );
-				if ( $ip === $ip_address ) {
-					return true;
-				}	
-				if ( false !== strpos( $ip, '*' ) ) {
-					$start = (float)sprintf( "%u", ip2long( trim( str_replace( '*', '0', $ip ) ) ) );
-					$end = (float)sprintf( "%u", ip2long( trim( str_replace( '*', '255', $ip ) ) ) );
-					if ( $ip_address_long >= $start && $ip_address_long <= $end ) {
-						return true;
-					}
-				}
-				if ( false !== stripos( $ip, 'x' ) ) {
-					$start = (float)sprintf( "%u", ip2long( trim( str_ireplace( 'x', '0', $ip ) ) ) );
-					$end = (float)sprintf( "%u", ip2long( trim( str_ireplace( 'x', '255', $ip ) ) ) );
-					if ( $ip_address_long >= $start && $ip_address_long <= $end ) {
-						return true;
-					}
-				}
-				if ( false !== stripos( $ip, '-' ) ) {
-					list( $start, $end ) = explode( '-', $ip, 2 );
-					$start = (float)sprintf( "%u", ip2long( trim( $start ) ) );
-					$end = (float)sprintf( "%u", ip2long( trim( $end ) ) );
-					if ( $ip_address_long >= $start && $ip_address_long <= $end ) {
-						return true;
-					}
-				}
-				if ( false !== strpos( $ip, '/' ) ) {
-					list( $net, $mask ) = explode( '/', $ip, 2 );
-					$net = ip2long( trim( $net ) );
-					$mask = ~( ( 1 << ( 32 - trim( $mask ) ) ) - 1 );
-					$ip_net = $ip_address_long & $mask;
-					if ( $ip_net === $net ) {
-						return true;
-					}
-				}
-			}
-
-			return false;
-
-		}
-		
-		/**
-		 * Get zeen101's Leaky Paywall - IP Exceptions options
-		 *
-		 * @since 1.0.0
-		 */
-		function get_settings() {
-			
-			$defaults = array( 
-				'allowed_ip_addresses' => '',
-			);
-		
-			$defaults = apply_filters( 'leaky_paywall_ip_exceptions_default_settings', $defaults );
-			
-			$settings = get_option( 'issuem-leaky-paywall-ip-exceptions' );
-												
-			return wp_parse_args( $settings, $defaults );
-			
-		}
-		
-		/**
-		 * Update zeen101's Leaky Paywall options
-		 *
-		 * @since 1.0.0
-		 */
-		function update_settings( $settings ) {
-			
-			update_option( 'issuem-leaky-paywall-ip-exceptions', $settings );
-			
-		}
-		
-		/**
-		 * Create and Display settings page
-		 *
-		 * @since 1.0.0
-		 */
-		function settings_div() {
-			
-			// Get the user options
-			$settings = $this->get_settings();
-			
-			// Display HTML form for the options below
-			?>
-            <div id="modules" class="postbox">
-            
-                <div class="handlediv" title="Click to toggle"><br /></div>
-                
-                <h3 class="hndle"><span><?php _e( 'IP Exceptions', 'issuem-lp-ipe' ); ?></span></h3>
-                
-                <div class="inside">
-                
-                <table id="leaky_paywall_ip_exceptions" class="form-table">
-                
-                    <tr>
-                        <th><?php _e( 'Allowed IP Addresses', 'issuem-lp-ipe' ); ?></th>
-                        <td>
-                        <textarea id="allowed_ip_addresses" class="regular-text code" cols="50" rows="10" name="allowed_ip_addresses"><?php echo $settings['allowed_ip_addresses']; ?></textarea>
-                        <p class="description"><?php printf( __( 'Examples: %s', 'issuem-lp-ipe' ), '<br />192.168.0.0<br />192.168.0.0-192.168.0.255<br />192.168.0.*<br />192.168.0.x<br />192.168.0.0/24' ); ?></p>
-                        </td>
-                    </tr>
-                    
-                </table>
-                                                                  
-                </div>
-                
-            </div>
-			<?php
-			
-		}
-		
-		function update_settings_div($settings, $current_tab) {
-			
-			if ( $current_tab !== 'subscriptions' ) {
-				return $settings;
-			}
-
-			// Get the user options
-			$ip_settings = $this->get_settings();
+	/**
+	 * Class constructor, puts things in motion
+	 *
+	 * @since 1.0.0
+	 */
+	function __construct() {
 				
-			if ( !empty( $_REQUEST['allowed_ip_addresses'] ) )
-				$ip_settings['allowed_ip_addresses'] = str_replace( ',', "\n", trim( $_REQUEST['allowed_ip_addresses'] ) );
-			else
-				$ip_settings['allowed_ip_addresses'] = '';
-			
-			$this->update_settings( $ip_settings );
+		$settings = $this->get_settings();
+		
+		add_filter( 'leaky_paywall_filter_is_restricted', array( $this, 'maybe_allow_access' ), 5, 3 );
+		add_action( 'leaky_paywall_after_subscriptions_settings', array( $this, 'settings_div' ) );
+		add_filter( 'leaky_paywall_update_settings_settings', array( $this, 'update_settings_div' ), 10, 2 );
+		
+	}
 
-			return $settings;
-			
+	public function maybe_allow_access( $is_restricted, $restriction_settings, $post_id ) 
+	{
+		if ( leaky_paywall_ip_allows_access() ) {
+			$is_restricted = false;
 		}
+		
+		return $is_restricted;	
+	}
+	
+	public function ip_allows_access() 
+	{
+		
+		$settings = $this->get_settings();
+		$ip_address = leaky_paywall_get_ip_address();
+		$ip_address_long = (float)sprintf( "%u", ip2long( $ip_address ) );
+		
+		$allowed_ips = explode( "\n", $settings['allowed_ip_addresses'] );
+
+		foreach( $allowed_ips as $ip ) {
+			$ip = trim( $ip );
+			if ( $ip === $ip_address ) {
+				return true;
+			}	
+			if ( false !== strpos( $ip, '*' ) ) {
+				$start = (float)sprintf( "%u", ip2long( trim( str_replace( '*', '0', $ip ) ) ) );
+				$end = (float)sprintf( "%u", ip2long( trim( str_replace( '*', '255', $ip ) ) ) );
+				if ( $ip_address_long >= $start && $ip_address_long <= $end ) {
+					return true;
+				}
+			}
+			if ( false !== stripos( $ip, 'x' ) ) {
+				$start = (float)sprintf( "%u", ip2long( trim( str_ireplace( 'x', '0', $ip ) ) ) );
+				$end = (float)sprintf( "%u", ip2long( trim( str_ireplace( 'x', '255', $ip ) ) ) );
+				if ( $ip_address_long >= $start && $ip_address_long <= $end ) {
+					return true;
+				}
+			}
+			if ( false !== stripos( $ip, '-' ) ) {
+				list( $start, $end ) = explode( '-', $ip, 2 );
+				$start = (float)sprintf( "%u", ip2long( trim( $start ) ) );
+				$end = (float)sprintf( "%u", ip2long( trim( $end ) ) );
+				if ( $ip_address_long >= $start && $ip_address_long <= $end ) {
+					return true;
+				}
+			}
+			if ( false !== strpos( $ip, '/' ) ) {
+				list( $net, $mask ) = explode( '/', $ip, 2 );
+				$net = ip2long( trim( $net ) );
+				$mask = ~( ( 1 << ( 32 - trim( $mask ) ) ) - 1 );
+				$ip_net = $ip_address_long & $mask;
+				if ( $ip_net === $net ) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+
+	}
+	
+	/**
+	 * Get zeen101's Leaky Paywall - IP Exceptions options
+	 *
+	 * @since 1.0.0
+	 */
+	function get_settings() {
+		
+		$defaults = array( 
+			'allowed_ip_addresses' => '',
+		);
+	
+		$defaults = apply_filters( 'leaky_paywall_ip_exceptions_default_settings', $defaults );
+		
+		$settings = get_option( 'issuem-leaky-paywall-ip-exceptions' );
+											
+		return wp_parse_args( $settings, $defaults );
+		
+	}
+	
+	/**
+	 * Update zeen101's Leaky Paywall options
+	 *
+	 * @since 1.0.0
+	 */
+	function update_settings( $settings ) {
+		
+		update_option( 'issuem-leaky-paywall-ip-exceptions', $settings );
+		
+	}
+	
+	/**
+	 * Create and Display settings page
+	 *
+	 * @since 1.0.0
+	 */
+	function settings_div() {
+		
+		// Get the user options
+		$settings = $this->get_settings();
+		
+		// Display HTML form for the options below
+		?>
+        <div id="modules" class="postbox">
+        
+            <div class="handlediv" title="Click to toggle"><br /></div>
+            
+            <h3 class="hndle"><span><?php _e( 'IP Exceptions', 'issuem-lp-ipe' ); ?></span></h3>
+            
+            <div class="inside">
+            
+            <table id="leaky_paywall_ip_exceptions" class="form-table">
+            
+                <tr>
+                    <th><?php _e( 'Allowed IP Addresses', 'issuem-lp-ipe' ); ?></th>
+                    <td>
+                    <textarea id="allowed_ip_addresses" class="regular-text code" cols="50" rows="10" name="allowed_ip_addresses"><?php echo $settings['allowed_ip_addresses']; ?></textarea>
+                    <p class="description"><?php printf( __( 'Examples: %s', 'issuem-lp-ipe' ), '<br />192.168.0.0<br />192.168.0.0-192.168.0.255<br />192.168.0.*<br />192.168.0.x<br />192.168.0.0/24' ); ?></p>
+                    </td>
+                </tr>
+                
+            </table>
+                                                              
+            </div>
+            
+        </div>
+		<?php
+		
+	}
+	
+	function update_settings_div($settings, $current_tab) {
+		
+		if ( $current_tab !== 'subscriptions' ) {
+			return $settings;
+		}
+
+		// Get the user options
+		$ip_settings = $this->get_settings();
+			
+		if ( !empty( $_REQUEST['allowed_ip_addresses'] ) )
+			$ip_settings['allowed_ip_addresses'] = str_replace( ',', "\n", trim( $_REQUEST['allowed_ip_addresses'] ) );
+		else
+			$ip_settings['allowed_ip_addresses'] = '';
+		
+		$this->update_settings( $ip_settings );
+
+		return $settings;
 		
 	}
 	
--- functions.php
+++ functions.php
@@ -4,43 +4,40 @@
  * @since 1.0.0
  */
  
-if ( !function_exists( 'leaky_paywall_get_ip_address' ) ) {
-		
-	function leaky_paywall_get_ip_address() {
-		//Just get the headers if we can or else use the SERVER global
-		if ( function_exists( 'apache_request_headers' ) ) {
-			$headers = apache_request_headers();
-		} else {
-			$headers = $_SERVER;
-		}
-		
-		//Get the forwarded IP if it exists
-		if ( array_key_exists( 'X-Forwarded-For', $headers ) &&
-			(
-				filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ||
-				filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6 ) )
-			) {
-				$the_ip = $headers['X-Forwarded-For'];
-		} elseif (
-			array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) &&
-			(
-				filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ||
-				filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6 )
-			)
-			) {
-			$the_ip = $headers['HTTP_X_FORWARDED_FOR'];
-		} else {
-			$the_ip = $_SERVER['REMOTE_ADDR'];
-		}
-		
-		if ( preg_match( '((\d+)\.(\d+)\.(\d+)\.(\d+)$)', $the_ip, $matches ) ) {
-			return $matches[0];
-		}
-		
-		return esc_sql( $the_ip );	
+function leaky_paywall_get_ip_address() {
+	//Just get the headers if we can or else use the SERVER global
+	if ( function_exists( 'apache_request_headers' ) ) {
+		$headers = apache_request_headers();
+	} else {
+		$headers = $_SERVER;
 	}
-
+	
+	//Get the forwarded IP if it exists
+	if ( array_key_exists( 'X-Forwarded-For', $headers ) &&
+		(
+			filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ||
+			filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6 ) )
+		) {
+			$the_ip = $headers['X-Forwarded-For'];
+	} elseif (
+		array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) &&
+		(
+			filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ||
+			filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6 )
+		)
+		) {
+		$the_ip = $headers['HTTP_X_FORWARDED_FOR'];
+	} else {
+		$the_ip = $_SERVER['REMOTE_ADDR'];
+	}
+	
+	if ( preg_match( '((\d+)\.(\d+)\.(\d+)\.(\d+)$)', $the_ip, $matches ) ) {
+		return $matches[0];
+	}
+	
+	return esc_sql( $the_ip );	
 }
+
 
 function leaky_paywall_ip_allows_access() {
 
